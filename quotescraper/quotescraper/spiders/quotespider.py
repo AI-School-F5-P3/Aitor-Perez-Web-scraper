33b45dcd49f79ad07a20d71d49329199b956fcea
Aitor Pérez <148784736+aitorph7@users.noreply.github.com>
2024-07-28 19:39:29 +0200

    feat: 'parse' function + incluyo archivos de resultado del spider *.json en '.gitignore'

--- quotescraper/quotescraper/spiders/quotespider.py
+++ quotescraper/quotescraper/spiders/quotespider.py
@@ -7,4 +7,17 @@ class QuotespiderSpider(scrapy.Spider):
     start_urls = ["https://quotes.toscrape.com/"]
 
     def parse(self, response):
-        pass
+        quotes = response.css('div.quote')
+        
+        for quote in quotes:
+            yield {
+                'quote': quote.css('span.text::text').get(),
+                'author': quote.css('small.author::text').get(),
+                'tags': quote.css('div.tags a.tag::text').getall(),
+                'about': quote.css('div.quote span a::attr(href)').get()
+            }
+            
+        next_page = response.css('li.next a::attr(href)').get()
+        if next_page:
+            url_next_page = 'https://quotes.toscrape.com' + next_page
+            yield response.follow(url_next_page, callback=self.parse)
