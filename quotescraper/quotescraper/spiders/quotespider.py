127a49487e244661533be41f7793907144125c85
Aitor Pérez <148784736+aitorph7@users.noreply.github.com>
2024-07-29 11:50:11 +0200

    perf: 'parse()' and 'parse_author()' mods to improve quotespider performance <- error: only 1 quote by auhor is obtained

--- quotescraper/quotescraper/spiders/quotespider.py
+++ quotescraper/quotescraper/spiders/quotespider.py
@@ -3,27 +3,42 @@ import scrapy
 
 class QuotespiderSpider(scrapy.Spider):
     name = "quotespider"
-    allowed_domains = ["quotes.toscrape.com"]
-    start_urls = ["https://quotes.toscrape.com/"]
+    start_urls = ["https://quotes.toscrape.com/", ]
 
     def parse(self, response):
-        quotes = response.css('div.quote')
         
-        for quote in quotes:
-            yield {
-                'quote': quote.css('span.text::text').get(),
-                'author': quote.css('small.author::text').get(),
-                'tags': quote.css('div.tags a.tag::text').getall(),
-                'about': quote.css('div.quote span a::attr(href)').get()
-            }
+        for quote in response.css('div.quote'):
+            # Extraer la info de cada cita
+            text = quote.css('span.text::text').get()
+            author = quote.css('small.author::text').get()
+            about_url = response.urljoin(quote.css('span > a::attr(href)').get())
+            tags = quote.css('div.tags a.tag::text').getall()
             
-        next_page = response.css('li.next a::attr(href)').get()
-        if next_page:
-            next_page_url = 'https://quotes.toscrape.com' + next_page
-            yield response.follow(next_page_url, callback=self.parse)
-
-    def parse_about(self, response, author):
+            # Extraer info de la cita y seguir el enlace del autor
+            yield response.follow(about_url, self.parse_author, meta={
+                'text': text, 
+                'author': author, 
+                'about': about_url,
+                'tags' : tags
+                })
+            
+        # Seguir a la siguiente página
+        next_page_url = response.css('li.next a::attr(href)').get()
+        if next_page_url is not None:
+            yield response.follow(next_page_url, self.parse)
+        
+    def parse_author(self, response):
+        text = response.meta['text']
+        author = response.meta['author']
+        about_url = response.meta['about']
+        tags = response.meta['tags']
+        
+        bio = response.css('div.author-description::text').get()
+        
         yield {
+            'text': text,
             'author': author,
-            'about': response.css('.author-description::text').get()
+            'about': about_url,
+            'tags': tags,
+            'bio': bio,
         }
